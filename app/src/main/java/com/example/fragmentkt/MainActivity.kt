97100f0dd686bad766b1e79a83638c8f485ef057
merajuni7ed <merajmusavi1243m@gmail.com>
2023-07-06 00:17:03 +0330

    update pager

--- app/src/main/java/com/example/fragmentkt/MainActivity.kt
+++ app/src/main/java/com/example/fragmentkt/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.fragmentkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.fragmentkt.databinding.ActivityMainBinding
 import com.example.fragmentkt.fragments.DolorFragment
 import com.example.fragmentkt.fragments.GoldFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,17 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.tablayout.getTabAt(1)?.select()
+        binding.tablayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                if (tab?.position == 0) {
+                    transaction.replace(R.id.frameFrag, GoldFragment()).commit()
+                } else {
+                    transaction.replace(R.id.frameFrag, DolorFragment()).commit()
+                }
 
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,DolorFragment()).commit()
 
-        binding.btnChange.setOnClickListener {
-            val currentFragment = supportFragmentManager.findFragmentById(R.id.frameLayout)
-            val newFragment = if (currentFragment is DolorFragment) GoldFragment() else DolorFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, newFragment).commit()
+            }
 
-        }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
 
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
 
-
+        })
     }
 }
